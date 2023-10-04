8238882154631257fa8efe5f509cf0ba638fc375
Reza Soltani <soltanireza65@gmail.com>
2023-10-04 14:42:23 +0330

    Add soukai-solid

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-const myCustomFunction = (name: string):string => `Hello ${name}`;
+const myCustomFunction = (name: string): string => `Hello ${name}`;
 function ciao(name: string): string {
     return `Ciao ${name}`;
 }
 
-export { myCustomFunction, ciao};
+export { myCustomFunction, ciao };
