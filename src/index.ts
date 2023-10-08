64bacf47b962877c6a76a1b68a0dde71f2b251a8
Reza Soltani <soltanireza65@gmail.com>
2023-10-08 19:07:19 +0330

    add Profile module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { Bookmark, BookmarkFactory, BookmarkSchema, IBookmark, ICreateBookmark } from './modules/Bookmarks';
+export { Profile, ProfileFactory, ProfileSchema, IProfile, ICreateProfile } from './modules/Profile';
